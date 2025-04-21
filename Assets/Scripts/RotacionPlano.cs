7a0af64d8ecb52e2ac41aaad406cf4872bead4cf
PaulaRuiz13 <paula.r.trivino@gmail.com>
2025-04-21 18:28:36 +0200

    Personaje nuevo

---
+++ Assets/Scripts/RotacionPlano.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionPlano : MonoBehaviour
+{
+    public Transform player; // Aqui se añade la camara que utilizamos como si fuera el "player"
+
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        direction.y = 0; // Ignora la rotación en el eje Y para conseguir que solo gire de manera horizontal
+
+        if (direction != Vector3.zero)
+        {
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            transform.rotation = rotation;
+        }
+    }
+
+    void start()
+    {
+        player= GameObject.FindGameObjectWithTag("MainCamera").transform; // Busca el objeto con la etiqueta "MainCamera" y lo asigna a la variable player
+    }
+}
