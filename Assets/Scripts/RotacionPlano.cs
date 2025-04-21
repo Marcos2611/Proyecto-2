500f4efbefe22ddc87b9e26b32bab61729c414d1
Daniel <dperezterron19@gmail.com>
2025-04-21 19:27:42 +0200

    DestroyNPC+bugfix

--- Assets/Scripts/RotacionPlano.cs
+++ Assets/Scripts/RotacionPlano.cs
@@ -1,26 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RotacionPlano : MonoBehaviour
 {
     public Transform player; // Aqui se añade la camara que utilizamos como si fuera el "player"
 
+    void Awake()
+    {
+        PlaneTextureManager.UiCaller += DestroyNPC;
+    }
+
     void Update()
     {
         Vector3 direction = player.position - transform.position;
-        direction.y = 0; // Ignora la rotación en el eje Y para conseguir que solo gire de manera horizontal
+
+  
+        direction.y = 0;
 
         if (direction != Vector3.zero)
         {
-            Quaternion rotation = Quaternion.LookRotation(direction);
-            transform.rotation = rotation;
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+           
+            transform.rotation = Quaternion.Euler(90, lookRotation.eulerAngles.y, 0);
         }
     }
 
     void Start()
     {
-        player= GameObject.Find("Main Camera").transform; 
+        player= GameObject.Find("Main Camera").transform;
+        transform.rotation = Quaternion.Euler(90, 0, 0);
+    }
 
+    private void DestroyNPC()
+    {
+        Destroy(this.gameObject);
     }
 }
